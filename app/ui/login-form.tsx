b55fe20ce270d8c710a67052882c0616f0e282fd
Simphiwe Nkabinde <simphiwe@shaper.co.za>
2026-05-14 16:45:39 +0200

    login page

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,33 +1,57 @@
-import Button from "./button";
-
+import Image from "next/image";
 
 export default function LoginForm() {
     return (
-        <form className="rounded-lg border border-gray-200 p-10 w-full">
-            <h1 className="text-xl font-semibold mb-5">Please log in to continue</h1>
-            <div className="w-full">
-                <div className="mb-5">
-                    <label
-                        className="mb-2 block text-sm font-medium test-gray-900"
-                        htmlFor="email">Email</label>
-                    <input
-                        className="block w-full rounded-md border border-gray-300 text-gray-900 p-2.5 placeholder:text-gray-500"
-                        type="email"
-                        placeholder="name@email.com"
-                        required />
-                </div>
-                <div className="mb-5">
-                    <label
-                        className="mb-3 block text-sm font-medium test-gray-900"
-                        htmlFor="password">Password</label>
-                    <input
-                        className="block w-full rounded-md border border-gray-300 text-gray-900 p-2.5 placeholder:text-gray-500"
-                        type="password" />
-                </div>
-                <Button>
-                    Log in
-                </Button>
+        <form className="flex w-full flex-col max-w-96">
+            <Image
+                src="/shaper-full-logo-horizontal.webp"
+                className="block"
+                width={1000}
+                height={500}
+                alt="shaper full logo" />
+            <p className="mb-5 text-ll font-medium">Learner Grades Dashboard</p>
+
+            <h1 className="text-4xl font-medium text-gray-900">Login</h1>
+
+            <p className="mt-4 text-base text-gray-500/90">
+                Please sign in to continue.
+            </p>
+
+            <div className="mt-10">
+                <label className="font-medium">Email</label>
+                <input
+                    placeholder="Please enter your email"
+                    className="mt-2 rounded-md ring ring-gray-200 focus:ring-2 focus:ring-indigo-600 outline-none px-3 py-3 w-full"
+                    required
+                    type="email"
+                    name="email"
+                />
             </div>
+
+            <div className="mt-6">
+                <label className="font-medium">Password</label>
+                <input
+                    placeholder="Please enter your password"
+                    className="mt-2 rounded-md ring ring-gray-200 focus:ring-2 focus:ring-indigo-600 outline-none px-3 py-3 w-full"
+                    required
+                    type="password"
+                    name="password"
+                />
+            </div>
+
+            <button
+                type="submit"
+                className="mt-8 py-3 w-full cursor-pointer rounded-md bg-indigo-600 text-white transition hover:bg-indigo-700"
+            >
+                Login
+            </button>
+            <p className='text-center py-8'>
+                Don't have an account? <a href="/signup" className="text-indigo-600 hover:underline">Sign up</a>
+            </p>
+            <button type="button" className="w-full flex items-center gap-2 justify-center my-3 bg-white border border-gray-500/30 py-2.5 rounded-md text-gray-800">
+                <Image width={45} height={45} className="h-4 w-4" src="/google-favicon.png" alt="googleFavicon" />
+                Log in with Google
+            </button>
         </form>
     )
 }
